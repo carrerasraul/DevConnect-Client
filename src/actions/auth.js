0d7bd9cb2e0260c193f3b5d28fba1968a9b81330
Raul Carreras <raul.e.carreras@gmail.com>
2020-04-28 01:57:07 -0400

    config

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -16,19 +16,17 @@ import setAuthToken from '../utils/setAuthToken';
 export const loadUser = () => async (dispatch) => {
   if (localStorage.token) {
     setAuthToken(localStorage.token);
-  }
-
-  try {
-    const res = await axios.get('/api/auth');
-
-    dispatch({
-      type: USER_LOADED,
-      payload: res.data,
-    });
-  } catch (err) {
-    dispatch({
-      type: AUTH_ERROR,
-    });
+    try {
+      const res = await axios.get('/api/auth');
+      dispatch({
+        type: USER_LOADED,
+        payload: res.data,
+      });
+    } catch (err) {
+      dispatch({
+        type: AUTH_ERROR,
+      });
+    }
   }
 };
 
